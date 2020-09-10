2264bfd63677862fa5beb720206f27a6b81a8917
Myriam <myriam.puchalt@ibm.com>
2020-09-10 23:51:32 +0100

    Player X won with a vertical line

--- scenarios.md
+++ scenarios.md
@@ -5,6 +5,12 @@
     • This Phase includes not only the board creation but also the messages that will show the progress of the creation
 
 2 - Player X won with a vertical line
+
+  • The game begins with player X, so we need to place 3 X's in the same column
+  • If we imagine the board as a 3x3 matrix (0-9 positions), the 3 vertical wining options are:
+      [0, 3, 6], [1, 4, 7], [2, 5, 8]
+  • If the 3 X's are in any of those 3 combinations of positions, the X wins with a vertical line  
+  
 3 – Player O won with a horizontal line
 4 – Player X won with a diagonal line
 5 – Game ends with a draw
--- ticTacToe.js
+++ ticTacToe.js
@@ -1,30 +1,62 @@
-const getPlayingRow = () => {
-    return ' | |';
+const getPlayingRow = (xPosition = [], oPosition = [], rowNumber = null) => {
+    if (xPosition.length > 1 && oPosition.length > 1) {
+        const matrixPositions = [[0,1,2],[3,4,5],[6,7,8]];
+        const matrixPositionsByRow = matrixPositions[rowNumber-1] || [];
+
+        const row = matrixPositionsByRow.map(position => {
+            if (xPosition.includes(position)) {
+                return 'X';
+            }
+            if (oPosition.includes(position)) {
+                return 'O';
+            } 
+            return ' ';
+        });
+        return row.join('|');
+    }
+    return ' | | ';
 }
+
 const getSeparationRow = () => {
     return '-+-+-';
 }
 
-const createBoard = () => {
-    const firstRow = getPlayingRow();
-    const secondRow = getSeparationRow();
-    const thirdRow = getPlayingRow();
-    const forthRow = getSeparationRow();
-    const fifthRow = getPlayingRow();
-
-    return `${firstRow}\n${secondRow}\n${thirdRow}\n${forthRow}\n${fifthRow}`;
-}
-
-const initBoard = () => {
-
+const initBoard = (player) => {
     const boardCreation = 'Game Board Creation...';
-    const board = createBoard();
+    const board = gameBoard();
     const boardCreated = 'Board Created';
-    const gameStart = 'The game will start with player X';
+    const gameStart = 'The game will start with player ' + player;
 
     return `${boardCreation}\n${board}\n${boardCreated}\n${gameStart}`;
 }
 
+const gameWinner = (xPosition) => {
+    const verticalWinningPositions = JSON.stringify([[0,3,6],[1,4,7],[2,5,8]]);
+    const winningPlayer = verticalWinningPositions.includes(JSON.stringify(xPosition)) ? 'X' : null;
+    return `PLAYER ${winningPlayer} WON!`;
+}
+
+const gameBoard = (xPosition, oPosition) => {
+
+    const firstRow = getPlayingRow(xPosition, oPosition, 1);
+    const secondRow = getSeparationRow();
+    const thirdRow = getPlayingRow(xPosition, oPosition, 2);
+    const forthRow = getSeparationRow();
+    const fifthRow = getPlayingRow(xPosition, oPosition, 3);
+
+    return `${firstRow}\n${secondRow}\n${thirdRow}\n${forthRow}\n${fifthRow}`;
+}
+
+const gameStarted = (player) => {
+    return `Player ${player}:`;
+}
+
+const gamePlayed = (player, xPosition, oPosition) => {
+    return `${gameStarted(player)}\n${gameBoard(xPosition, oPosition)}\n${gameWinner(xPosition, oPosition)}`;
+}
+
 module.exports = {
-    initBoard
+    initBoard,
+    gameWinner,
+    gamePlayed
 };
